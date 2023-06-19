360a0df7a764bf56c676a4b6ba1e3107c18b35b9
OsaidAttar <osedatar2017@gmail.com>
2023-06-20 02:01:20 +0300

    ll

--- src/module/user/user.router.js
+++ src/module/user/user.router.js
@@ -2,10 +2,10 @@ import {Router} from 'express'
 import * as userController from './controller/user.controller.js'
 import { auth } from '../middelware/auth.middelware.js'
 import { asyncHandler } from '../../services/errorHandling.js'
-import fileUpload, { HME } from '../../services/multer.js'
+import fileUpload, { HME, fileValidation } from '../../services/multer.js'
 const router =Router()
 router.get('/profile',auth,asyncHandler(userController.profile))
-//router.patch('/profilePicture',auth,fileUpload(['image/png','image/jpeg','image/gif']).single('image'),HME,asyncHandler(userController.profilePicture))
-router.patch('/profilePicture',auth,fileUpload(['application/pdf']).single('image'),HME,asyncHandler(userController.profilePicture))
+router.patch('/profilePicture',auth,fileUpload(fileValidation.image).single('image'),HME,asyncHandler(userController.profilePicture))
+//router.patch('/profilePicture',auth,fileUpload(['application/pdf']).single('image'),HME,asyncHandler(userController.profilePicture))
 
 export default router
--- src/services/multer.js
+++ src/services/multer.js
@@ -6,6 +6,10 @@ return res.status(400).json({message:"multer error"})
     next()
 }
 }
+ export const fileValidation={
+    image:['image/png','image/jpeg','image/gif'],
+    file:['application/pdf']
+}
 function fileUpload(custumValidation=[]){
     const storage=multer.diskStorage({})
 
